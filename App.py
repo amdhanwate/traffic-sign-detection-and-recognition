1752229798511f38f58327c965499234e16de645
= <=>
2022-11-17 14:31:11 +0530

    deploy phase 1

--- App.py
+++ App.py
@@ -6,21 +6,32 @@ import cv2
 import numpy as np
 import pandas as pd
 
+with open("./css/app.css", "r") as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
 model_dir = r"C:\Users\SAI\Desktop\Traffic Sign Detectiona and Recognition\trained models\pedestrian"
 
-knn_model = pickle.load(open(os.path.join(model_dir, "model_knn.sav"), "rb"))
+models = {
+    "KNN": pickle.load(open(os.path.join(model_dir, "model_knn.sav"), "rb")),
+    "lr_model": pickle.load(open(os.path.join(model_dir, "Model_LOG_REG.sav"), "rb")),
+    "svm_model": pickle.load(open(os.path.join(model_dir, "PCA_3_Model.sav"), "rb")),
+    "rf_model": pickle.load(open(os.path.join(model_dir, "model_rf.sav"), "rb")),
+    "svc_model": pickle.load(open(os.path.join(model_dir, "SVC_RBF_PCA_3.sav"), "rb")),
+    "dt_model": pickle.load(open(os.path.join(model_dir, "model1.sav"), "rb")),
+    "kmeans_model": pickle.load(open(os.path.join(model_dir, "Kmeans_CL_5_Model.sav"), "rb"))
+}
 
-def welcome():
-    return "Welcome to Traffic Sign Detection and Recognition"
+models_options = ["KNN", "lr_model", "svm_model", "rf_model", "svc_model", "dt_model", "kmeans_model"]
+models_display = ["KNN", "Logistic Regression", "SVM", "Random Forest", "SVC", "Decision Tree", "K-Means"]
 
-def processImg(img_path):
-    print(img_path)
-    # i = cv2.imread(img_path)
-    # cv2.imshow("Image", i)
-    # cv2.waitKey(0) 
-  
-    # #closing all open windows 
-    # cv2.destroyAllWindows() 
+# def workKNN()
+
+
+def setModel(model):
+    return models[model]
+
+def processImg(model, img_path):
+    st.write(model)
     try:
         path = os.path.join(img_path)
         a = cv2.imread(path)
@@ -34,7 +45,7 @@ def processImg(img_path):
         
         try:
             array_double = np.array(out, dtype=np.double)
-            a = knn_model.predict(array_double)
+            a = setModel(model).predict(array_double)
             hist=np.histogram(a,bins=[0,1,2,3,4,5])
             print("HISTOGRAM",hist)
             return a
@@ -47,35 +58,41 @@ def processImg(img_path):
         print(str(e))
         return "Error"
 
-def main(img_path):
-    img = processImg(img_path).tolist()
+def main(model, img_path):
+    img = processImg(model, img_path).tolist()
     # print(img)
     return pd.DataFrame([
         ["Pedestrian", img.count(1)/(img.count(0)+img.count(1))*100],
         ["Other", img.count(0)/(img.count(0)+img.count(1))*100]
-    ], columns=["Sign", "Accuracy"])
-    # if (img.count(0) > img.count(1)):
-    #     return "Not a Pedestrian"
-    # else:
-    #     return ("Pedestrian: ", img.count(0)/(img.count(0)+img.count(1))*100, "%")
+    ], columns=["Sign", "Probablity (%)"])
 
 if __name__ == '__main__':
     st.title("Traffic Sign Detection and Recognition")
-    # img_path = st.text_input("Enter the image path")
+
+    model = st.selectbox(
+        "Select Model", 
+        models_options,
+        format_func=lambda x: models_display[models_options.index(x)]
+    )
+
     file = st.file_uploader("Upload the image", type=["jpg", "png"], label_visibility="collapsed")
-    # if st.button("Submit"):
-    #     result = main(img_path)
-    #     st.table(result) 
 
-    if file is not None:
-        st.write(file.name)
-        data = file.getvalue()
+    if st.button("Submit"):
+        if file is None:
+            st.warning("Please upload an image")
 
-        with open(f"./uploaded/{file.name}", "wb") as f:
-            f.write(data)
-        
-        result = main(f"./uploaded/{file.name}")
-        st.table(result) 
-    
-    else:
-        st.write("No file uploaded")
+        if file is not None:
+            try:
+                data = file.getvalue()
+
+                with open(f"./uploaded/{file.name}", "wb") as f:
+                    f.write(data)
+                
+                result = main(model, f"./uploaded/{file.name}")
+                st.image(Image.open(f"./uploaded/{file.name}"), width=400)
+
+                st.table(result)
+
+            except Exception as e:
+                st.write("Something went wrong..! Please try again..!")
+                st.write("Error: ", e)
